ac5b0e52fd06470a40893528aef795d4f0c7f84b
Pietro Battiston <me@pietrobattiston.it>
2017-06-02 11:19:08 +0200

    Removed trailing spaces

--- crypto/__init__.py
+++ crypto/__init__.py
@@ -12,7 +12,7 @@ ACTIONS = {'encrypt' : _("Encrypt document"),
 class GeditCrypto(GObject.Object, Gedit.WindowActivatable):
     __gtype_name__ = "CryptoPlugin"
     window = GObject.property(type=Gedit.Window)
-    
+
     def do_activate(self):
         try:
             self.initialize()
@@ -20,28 +20,28 @@ class GeditCrypto(GObject.Object, Gedit.WindowActivatable):
             import traceback
             print("Error initializing \"Crypto\" plugin")
             print(traceback.print_exc())
-    
+
     def initialize(self):
         from .crypto_ui import Ui
-        
+
         self.data_dir = self.plugin_info.get_data_dir()
-        
+
         ui_path = os.path.join( self.data_dir, "crypto.glade" )
         self.ui = Ui( "gedit-crypto", ui_path )
         self.ui.connect_signals( self )
-        
+
         self.actions = {}
-        
+
         for action_name in ACTIONS:
             action = Gio.SimpleAction(name=action_name)
             self.actions[action_name] = action
             action.connect('activate', getattr(self, action_name))
             self.window.add_action(action)
             self.window.lookup_action(action_name).set_enabled(True)
-        
+
         # Build encrypter when needed to not slow down Gedit startup
         self.enc = None
-    
+
     def do_deactivate(self):
         """
         Remove actions.
@@ -49,43 +49,43 @@ class GeditCrypto(GObject.Object, Gedit.WindowActivatable):
         while self.actions:
             name, action = self.actions.popitem()
             self.window.remove_action(name)
-    
+
     def do_update_state(self):
         pass
-    
+
     def encrypt(self, *args):
         if self.enc == None:
             self.enc = Encrypter( self.ui )
-        
+
         cleartext = self.get_current_text()
-        
+
         encrypted = self.enc.encrypt( cleartext )
-        
+
         if not encrypted:
             return
-        
+
         self.show_in_new_document( encrypted )
-    
+
     def decrypt(self, *args):
         if self.enc == None:
             self.enc = Encrypter( self.ui )
-        
+
         encrypted_text = self.get_current_text()
-        
+
         cleartext = self.enc.decrypt( encrypted_text )
-        
+
         if not cleartext:
             return
-        
+
         self.show_in_new_document( cleartext )
-        
+
     def get_current_text(self):
         view = self.window.get_active_view()
         doc = view.get_buffer()
         start = doc.get_start_iter()
         end = doc.get_end_iter()
         return doc.get_text( start, end, False )
-    
+
     def show_in_new_document(self, text):
         self.window.create_tab( True )
         new_view = self.window.get_active_view()
@@ -96,16 +96,16 @@ class GeditCrypto(GObject.Object, Gedit.WindowActivatable):
 class GeditCryptoApp(GObject.Object, Gedit.AppActivatable):
     __gtype_name__ = "GeditCryptoApp"
     app = GObject.property(type=Gedit.App)
-    
+
     def do_activate(self):
         self.submenu_ext = self.extend_menu("tools-section-1")
         submenu = Gio.Menu()
         item = Gio.MenuItem.new_submenu(_("Encrypt/decrypt"), submenu)
         self.submenu_ext.append_menu_item(item)
-        
+
         for action in ACTIONS:
             item = Gio.MenuItem.new(ACTIONS[action], "win.%s" % action)
             submenu.append_item(item)
-    
+
     def do_deactivate(self):
         del self.submenu_ext
--- crypto/encrypter.py
+++ crypto/encrypter.py
@@ -7,39 +7,39 @@ class Encrypter(object):
         self.ui = ui
         self.bus = dbus.SessionBus()
         self.init_dbus()
-            
+
     def init_dbus(self):
         keys_proxy = self.bus.get_object('org.gnome.seahorse', '/org/gnome/seahorse/keys')
         key_service = dbus.Interface(keys_proxy, 'org.gnome.seahorse.KeyService')
-        
+
         types = key_service.GetKeyTypes()
-        
+
         path = key_service.GetKeyset(types[0])
-        
+
         proxy_obj = self.bus.get_object('org.gnome.seahorse', path)
         self.keyset = dbus.Interface(proxy_obj, "org.gnome.seahorse.Keys")
-    
+
     def select_key(self):
         self.populate_keys_list()
-        
+
         resp = self.ui.main.run()
         self.ui.main.hide()
         if resp != 1:
             return
         return list( self.shown[self.ui.key_selection.get_selected()[1]] )
-    
+
     def populate_keys_list(self):
         keys = self.keyset.ListKeys()
-        
+
         self.shown = Gtk.TreeModelFilter( child_model=self.ui.keys )
         self.shown.set_visible_func( self.show_key, None )
-                
+
         self.ui.keys_view.set_model( self.shown )
         self.ui.search.connect( "changed", (lambda x : self.shown.refilter()) )
         self.ui.key_selection.connect( "changed", self.activate_OK_button )
-        
+
         self.ui.keys.clear()
-        
+
         fields_names = [ "display-name", "display-id", "fingerprint" ]
         # Fields are well described in
         # https://lug.asprion.org/wiki/1/Seahorse_DBUS_Interface
@@ -47,9 +47,9 @@ class Encrypter(object):
         # those).
         # Never found: "expires","enc-type"
         # "display-id" always the same as "raw-id"
-        
+
         duplicates = set()
-        
+
         for key in keys:
             if key.count(':') == 2:
                 # The correct format is e.g. "openpgp:0123456789ABCDE0";
@@ -70,26 +70,26 @@ class Encrypter(object):
                                      key])
         warnings.warn("Discarded the following keys, apparently duplicates:\n"
                       "%s" % duplicates)
-    
+
     def show_key(self, store, the_iter, data):
         search = self.ui.search.get_text()
         if not search:
             # No search currently active
             return True
-        
+
         return search.lower() in self.ui.keys[the_iter][3]
-    
+
     def activate_OK_button(self, selection):
         self.ui.OK_button.set_sensitive( bool( selection.get_selected()[1] ) )
-    
+
     def encrypt(self, cleartext):
         self.chosen = self.select_key()
         if not self.chosen:
             return
-        
+
         cr_proxy = self.bus.get_object('org.gnome.seahorse', '/org/gnome/seahorse/crypto')
         cr_service = dbus.Interface(cr_proxy, 'org.gnome.seahorse.CryptoService')
-        
+
         try:
             key = self.chosen[-1]
             encrypted = cr_service.EncryptText([key], "", 0, cleartext)
@@ -100,11 +100,11 @@ class Encrypter(object):
             self.ui.error.format_secondary_text( str(msg) )
             self.ui.error.run()
             self.ui.error.hide()
-    
+
     def decrypt(self, encrypted_text):
         cr_proxy = self.bus.get_object('org.gnome.seahorse', '/org/gnome/seahorse/crypto')
         cr_service = dbus.Interface(cr_proxy, 'org.gnome.seahorse.CryptoService')
-        
+
         try:
             # Notice "signer" is not returned:
             cleartext, signer = cr_service.DecryptText( "openpgp", 0, encrypted_text )
