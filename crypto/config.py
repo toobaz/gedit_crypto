3fd4d994d1e470f087d00574de0fa694dd695a15
Mubashshir <ahmubashshir@gmail.com>
2020-12-30 18:54:39 +0600

    auto compile schemas

--- crypto/config.py
+++ crypto/config.py
@@ -1,21 +1,25 @@
 from gi.repository import Gio
-from os.path import expanduser
+from os import path
 
 # FIXME: detect
 SYSTEM_INSTALL = False
 
 SCHEMA_ID = "org.gnome.gedit.plugins.crypto"
 
+
 def load_config():
     if SYSTEM_INSTALL:
         settings = Gio.Settings.new(SCHEMA_ID)
     else:
-        # TODO: install and compile automatically
-        schema_path = expanduser("~/.local/share/gedit-crypto/schemas")
+        import subprocess
+
+        schema_path = path.join(path.dirname(__file__), "schemas")
+        subprocess.call(['glib-compile-schemas', schema_path])
+
         schema_source = Gio.SettingsSchemaSource.new_from_directory(
-                                    schema_path,
-                                    Gio.SettingsSchemaSource.get_default(),
-                                    False)
+            schema_path,
+            Gio.SettingsSchemaSource.get_default(),
+            False)
         schema = schema_source.lookup(SCHEMA_ID, False)
         settings = Gio.Settings.new_full(schema, None, None)
     return settings
